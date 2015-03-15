97a5b9d251459e0e56e120a9a23f10acd50b1125
Eric Chen <eriolchan@gmail.com>
2015-03-15 22:03:48 +0800

    update chat.js and add twitter.js and test case

---
+++ nodejs/node-up-and-running/chapter-2/twitter.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    bodyParser = require('body-parser')
+
+var app = express()
+app.listen(8000)
+
+var tweets = []
+
+app.get('/', function(req, res) {
+    res.send('Welcome to Node Twitter')
+})
+
+app.post('/send', bodyParser.urlencoded({ extended:false }), function(req, res) {
+    if (req.body && req.body.tweet) {
+        tweets.push(req.body.tweet)
+        res.send({status:"ok", message:"Tweet received"})
+    } else {
+        res.send({status:"nok", message:"No tweet received"})
+    }
+})
+
+app.get('/tweets', function(req, res) {
+    res.send(tweets)
+})
+
+console.log('Start twitter service')
